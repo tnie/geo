5d27f1a701026cfd60d215c39a7f82441c7a6fbb
niel <pc@1502.xly>
2023-03-12 11:26:53 +0800

    自定义经纬度行编辑控件

--- glineedit.cpp
+++ glineedit.cpp
@@ -1,7 +1,58 @@
 #include "glineedit.h"
+#include <cmath>
 
 GLineEdit::GLineEdit(QWidget *parent)
     : QLineEdit{parent}
 {
+    connect(this, &QLineEdit::textEdited, this, &GLineEdit::on_textEdited);
+}
 
+double GLineEdit::getValue() const
+{
+    return value;
+}
+
+void GLineEdit::setValue(double newValue)
+{
+    value = newValue;
+    if(!std::isnan(value))
+    {
+        const QString strVal = fmt(value);
+        return setText(strVal);
+    }
+    setText("==°==。===′");
+}
+
+void GLineEdit::reset()
+{
+    setValue(std::nan("NaN"));
+}
+
+QString GLineEdit::fmt(const double value)
+{
+    return QString::number(value);
+}
+
+double GLineEdit::fmt(const QString &strVal)
+{
+    bool ok = false;
+    const double v = strVal.toDouble(&ok);
+    return ok ? v : std::nan("NaN");
+}
+
+void GLineEdit::on_textEdited(const QString &text)
+{
+    value = fmt(text);
+}
+
+
+GLonLineEdit::GLonLineEdit(QWidget *parent)
+    : GLineEdit{parent}
+{
+
+}
+
+QString GLonLineEdit::fmt(const double value)
+{
+    return "TODO";
 }
--- glineedit.h
+++ glineedit.h
@@ -9,8 +9,30 @@ class GLineEdit : public QLineEdit
 public:
     explicit GLineEdit(QWidget *parent = nullptr);
 
+    double getValue() const;
+    void setValue(double newValue);
+    void reset();
+
+protected:
+    virtual QString fmt(const double value);
+    virtual double fmt(const QString& strVal);
 signals:
 
+private:
+//    避免格式化输出后丢失精度
+    double value;
+    void on_textEdited(const QString& text);
+};
+
+class GLonLineEdit: public GLineEdit
+{
+    Q_OBJECT
+public:
+    explicit GLonLineEdit(QWidget *parent = nullptr);
+
+    static QString fmt(const double value, const QString fmt);
+protected:
+    QString fmt(const double value) override;
 };
 
 #endif // GLINEEDIT_H
